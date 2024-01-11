ce6c36a069c68236b5d937608153534474411a83
Robert Gutierrez <robert@ragutierrez.com>
2024-01-10 18:26:27 -0800

    checkpoint commit. first draft of full app

--- apps/flask_app/app.py
+++ apps/flask_app/app.py
@@ -5,10 +5,11 @@ from flask import Flask, request
 from flask_cors import CORS
 
 import psycopg2
+import psycopg2.extras
 import requests
 
 
-AIRFLOW_ENDPOINT_URL = "http://localhost:8080/api/v1"
+AIRFLOW_ENDPOINT_URL = "http://airflow-webserver:8080/api/v1"
 airflow_api_params = {
     'headers': {
         'Content-type': 'application/json',
@@ -34,8 +35,10 @@ def create_app():
         try:
             resp = requests.post(
                 f"{AIRFLOW_ENDPOINT_URL}/dags/process_sensor_data/dagRuns",
+                json={'conf': {}},
                 **airflow_api_params
             )
+            print(repr(resp.json()))
         except:
             logging.exception('Unable to trigger DAG!')
             return {'status': 'failed'}, 500
@@ -43,7 +46,19 @@ def create_app():
 
     @app.route('/get-current-data', methods=['GET'])
     def get_current_data():
-        return {}
+        pg_user = os.getenv('POSTGRES_USER')
+        pg_pw = os.getenv('POSTGRES_PASSWORD')
+        db = os.getenv('SENSOR_DATA_DB')
+        conn = psycopg2.connect(
+            user=pg_user,
+            password=pg_pw,
+            host="postgres",
+            dbname=db
+        )
+        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cur.execute('SELECT * FROM sensor_data');
+        results = [ row for row in cur ]
+        return results
         
     return app
     
--- apps/react_app/src/App.js
+++ apps/react_app/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 function App() {
-  const [dataDescription, setDataDescription] = useState('No data yet. Data will be available once it has been processed by Airflow.');
+  const [data, setData] = useState(null);
   const [flashMessage, setFlashMessage] = useState('');
   const [flashClass, setFlashClass] = useState('flash-message');
 
@@ -18,13 +18,15 @@ function App() {
 
   const handleRefreshData = (e) => {
     clearFlash();
-    setDataDescription('Loading...');
     axios
       .get('http://localhost:8000/get-current-data')
       .then((res) => {
-        // do something with data
         let data = res.data;
-        setDataDescription(data);
+        setData(data);
+        setFlash('Current data retrieved successfully!');
+        setTimeout(() => {
+          clearFlash();
+        }, 5000);
       })
       .catch((error) => {
         if (error.response) {
@@ -35,7 +37,7 @@ function App() {
           console.log("Error: ", error.message);
         }
         setFlash('An error occurred while refreshing data! Please try again.');
-        setDataDescription('No data yet.');
+        setData(null);
       });
   }
   const handleTriggerDAG = (e) => {
@@ -43,7 +45,11 @@ function App() {
     axios
       .get('http://localhost:8000/trigger')
       .then((res) => {
-        console.log('DAG trigger successful!')
+        console.log('DAG trigger successful!');
+        setFlash('DAG trigger successful!');
+        setTimeout(() => {
+          clearFlash();
+        }, 5000);
       })
       .catch((error) => {
         if (error.response) {
@@ -59,13 +65,52 @@ function App() {
   
   return (
     <div className="container">
-      <p class="flash-message">{flashMessage}</p>
+      <p className={flashClass}>{flashMessage}</p>
       <h1>Sample Sensor Data "Dashboard" - Robert Gutierrez</h1>
       <br />
-      <button onClick={() => handleTriggerDAG()}>Trigger DAG</button>
+      <button onClick={() => handleTriggerDAG()}>Trigger DAG</button>&nbsp;&nbsp;
+      <button onClick={() => handleRefreshData()}>Refresh Data</button>
       <br />
       <h4>Current Data</h4>
-      <div>{dataDescription}</div>
+      <div>{!data ? (<p>No data yet. Data will be available once it has been processed by Airflow.</p>) : (
+        <>
+          <small>Terminology: UFP = Ultrafine Particles, BC = Black Carbon, NO2 = Nitrogen Dioxide</small>
+          <table>
+            <thead>
+              <tr>
+                <th>Calculated at</th>
+                <th>UFP Mean</th>
+                <th>UFP Median</th>
+                <th>UFP Standard Deviation</th>
+                <th>BC Mean</th>
+                <th>BC Median</th>
+                <th>BC Standard Deviation</th>
+                <th>NO2 Mean</th>
+                <th>NO2 Median</th>
+                <th>NO2 Standard Deviation</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((row) => {
+                return (
+                  <tr key={row.id}>
+                    <td>{row.created_at}</td>
+                    <td>{parseFloat(row.ufp_mean).toFixed(2)}</td>
+                    <td>{parseFloat(row.ufp_median).toFixed(2)}</td>
+                    <td>{parseFloat(row.ufp_stddev).toFixed(2)}</td>
+                    <td>{parseFloat(row.bc_mean).toFixed(2)}</td>
+                    <td>{parseFloat(row.bc_median).toFixed(2)}</td>
+                    <td>{parseFloat(row.bc_stddev).toFixed(2)}</td>
+                    <td>{parseFloat(row.no2_mean).toFixed(2)}</td>
+                    <td>{parseFloat(row.no2_median).toFixed(2)}</td>
+                    <td>{parseFloat(row.no2_stddev).toFixed(2)}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
+      )}</div>
     </div>
   );
 }
