8ab363f93c099b98a05b2277541f674f0fa3e23f
Robert Gutierrez <robert@ragutierrez.com>
2024-01-10 17:27:10 -0800

    set up airflow api, start web app routes

--- apps/flask_app/app.py
+++ apps/flask_app/app.py
@@ -1,10 +1,24 @@
 import logging
+import os
 
 from flask import Flask, request
 from flask_cors import CORS
 
-UPLOAD_FOLDER = '/tmp/uploads'
+import psycopg2
+import requests
 
+
+AIRFLOW_ENDPOINT_URL = "http://localhost:8080/api/v1"
+airflow_api_params = {
+    'headers': {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+    },
+    'auth': requests.auth.HTTPBasicAuth(
+        os.getenv('_AIRFLOW_WWW_USER_USERNAME'),
+        os.getenv('_AIRFLOW_WWW_USER_PASSWORD')
+    )
+}
 
 def create_app():
     app = Flask(__name__)
@@ -14,7 +28,18 @@ def create_app():
     @app.route("/")
     def hello():
         return "Hello, World!"
-
+    
+    @app.route("/trigger", methods=['GET'])
+    def trigger_dag():
+        try:
+            resp = requests.post(
+                f"{AIRFLOW_ENDPOINT_URL}/dags/process_sensor_data/dagRuns",
+                **airflow_api_params
+            )
+        except:
+            logging.exception('Unable to trigger DAG!')
+            return {'status': 'failed'}, 500
+        return {"status": "success"}
 
     @app.route('/get-current-data', methods=['GET'])
     def get_current_data():
--- apps/react_app/src/App.js
+++ apps/react_app/src/App.js
@@ -4,16 +4,27 @@ import axios from 'axios';
 
 function App() {
   const [dataDescription, setDataDescription] = useState('No data yet. Data will be available once it has been processed by Airflow.');
+  const [flashMessage, setFlashMessage] = useState('');
+  const [flashClass, setFlashClass] = useState('flash-message');
 
-  const handleUpload = (e) => {
+  const clearFlash = () => {
+    setFlashMessage('');
+    setFlashClass('flash-message');
+  }
+  const setFlash = (msg) => {
+    setFlashMessage(msg);
+    setFlashClass('flash-message has-message');
+  }
+
+  const handleRefreshData = (e) => {
+    clearFlash();
     setDataDescription('Loading...');
     axios
       .get('http://localhost:8000/get-current-data')
       .then((res) => {
-        console.log('Axios GET request successful!')
         // do something with data
-        let desc = "Data";
-        setDataDescription(desc);
+        let data = res.data;
+        setDataDescription(data);
       })
       .catch((error) => {
         if (error.response) {
@@ -23,16 +34,38 @@ function App() {
         } else {
           console.log("Error: ", error.message);
         }
-        setDataDescription('An error occurred; unable to process! Please try again.')
+        setFlash('An error occurred while refreshing data! Please try again.');
+        setDataDescription('No data yet.');
+      });
+  }
+  const handleTriggerDAG = (e) => {
+    clearFlash();
+    axios
+      .get('http://localhost:8000/trigger')
+      .then((res) => {
+        console.log('DAG trigger successful!')
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log("Error: ", error.response.data.message);
+        } else if (error.request) {
+          console.log("Error: ", error.request);
+        } else {
+          console.log("Error: ", error.message);
+        }
+        setFlash('An error occurred while trying to trigger the DAG! Please try again.')
       });
   }
   
   return (
     <div className="container">
+      <p class="flash-message">{flashMessage}</p>
       <h1>Sample Sensor Data "Dashboard" - Robert Gutierrez</h1>
       <br />
+      <button onClick={() => handleTriggerDAG()}>Trigger DAG</button>
+      <br />
       <h4>Current Data</h4>
-      <p>No data yet.</p>
+      <div>{dataDescription}</div>
     </div>
   );
 }
